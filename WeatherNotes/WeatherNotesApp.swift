b1757e82c63e2e429b115c84e33603c729e4da07
kurog1ri <kosenko.yosiki@gmail.com>
2026-02-26 18:00:45 +0200

    Initial Commit

---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by   Kosenko Mykola on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
