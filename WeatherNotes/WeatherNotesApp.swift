5c84ed6bae9fc5193c549d228dfef7d3d29e9b18
kurog1ri <kosenko.yosiki@gmail.com>
2026-02-26 18:43:05 +0200

    feat: add dark mode support via preferredColorScheme

--- WeatherNotes/WeatherNotesApp.swift
+++ WeatherNotes/WeatherNotesApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct WeatherNotesApp: App {
+
+    @AppStorage("colorScheme") private var colorSchemePreference: String = "system"
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(resolvedColorScheme)
+        }
+    }
+
+    private var resolvedColorScheme: ColorScheme? {
+        switch colorSchemePreference {
+        case "light": return .light
+        case "dark": return .dark
+        default: return nil
         }
     }
 }
